476c937625fd0d9314a7c0021526bae402977350
von.stew <andrew.taylor.stewart@gmail.com>
2020-08-01 18:30:22 -0400

    check, validaionResult addition

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 // @route   GET api/users
-// @desc    TEST route
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    //check for name
+    check('name', 'Name is Required').not().isEmpty(),
+    //check for email
+    check('email', 'Please enter a valid email').isEmail(),
+    //check for password
+    check(
+      'password',
+      'Please enter a password with 6 or mote characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('Passed!');
+  }
+);
 
 module.exports = router;
