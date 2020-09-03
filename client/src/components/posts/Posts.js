32b95a1a4ac8328f6087d0270a8915dcd4231ab2
von.stew <andrew.taylor.stewart@gmail.com>
2020-09-02 22:21:42 -0400

    Posts in the community

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -9,11 +10,28 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     getPosts();
   }, [getPosts]);
 
-  return <Fragment></Fragment>;
+  console.log(posts);
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'> Welcome to the community</i>
+      </p>{' '}
+      {/*PostForm */}{' '}
+      <div className='posts'>
+        {' '}
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
-  getPosts: PropTypes.func,
+  getPosts: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired
 };
 
